02eb4b2c05462cfbe4d1e034886df20d4fdea2b8
Your Name <you@example.com>
2025-02-04 19:40:57 +0530

    pushing into git

---
+++ src/App.js
@@ -0,0 +1,80 @@
+// import React from "react";
+
+
+// function App() {
+//   let ele = React.createElement("div" , {className:"App"} ,
+//     React.createElement("h1" ,null, "Header")
+//   )
+//   return (
+//     <div className="App">
+//       <h1>Header</h1>
+//       <h1 style ={{color:"red"}}>one waste fellow vysh</h1>
+//     </div>
+  
+//   );
+// }
+
+// export default App;
+
+
+// !components 
+// !CBC
+
+// import React from "react";
+// calss App extends React.Component{
+
+// }
+
+
+// !compnents composition
+
+
+// import React from 'react'
+// import Navbar from '/components/navbar'
+// import Main from '/components/main'
+// import Sidebar1 from '/components/sidebar1'
+// import Sidebar2 from '/components/sidebar2'
+// import Footer from '/components/Footer'
+// const App = () => {
+//     return (
+
+//         <div className= 'App'>
+//             <navbar/>
+//             <main/>
+//         <div className='Sidebar'>
+//         <Sidebar1/>
+//         <Sidebar2/>
+
+//         </div>
+//         <Footer/>
+
+//         </div>
+
+    
+//     )
+// }
+// export default App
+import React from 'react';
+import Navbar from './components/navbar';  // Use lowercase 'navbar' if the file is named that way
+  // Assuming components are in the 'components' folder
+import Main from './components/Main';
+import Sidebar1 from './components/Sidebar1';
+import Sidebar2 from './components/Sidebar2';
+import Footer from './components/Footer';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <Navbar />
+            <Main />
+            <div className='Sidebar'>
+                <Sidebar1 />
+                <Sidebar2 />
+            </div>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
+
