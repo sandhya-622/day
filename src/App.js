d2633f5a45676b479950c9ad057dcc9feded0a62
Your Name <you@example.com>
2025-02-05 20:05:58 +0530

    pushing into git

--- src/App.js
+++ src/App.js
@@ -54,27 +54,58 @@
 //     )
 // }
 // export default App
-import React from 'react';
-import Navbar from './components/navbar';  // Use lowercase 'navbar' if the file is named that way
-  // Assuming components are in the 'components' folder
-import Main from './components/Main';
-import Sidebar1 from './components/Sidebar1';
-import Sidebar2 from './components/Sidebar2';
-import Footer from './components/Footer';
+// import React from 'react';
+// import Navbar from './components/navbar';  // Use lowercase 'navbar' if the file is named that way
+//   // Assuming components are in the 'components' folder
+// import Main from './components/Main';
+// import Sidebar1 from './components/Sidebar1';
+// import Sidebar2 from './components/Sidebar2';
+// import Footer from './components/Footer';
 
-const App = () => {
+// const App = () => {
+//     return (
+//         <div className='App'>
+//             <Navbar />
+//             <Main />
+//             <div className='Sidebar'>
+//                 <Sidebar1 />
+//                 <Sidebar2 />
+//             </div>
+//             <Footer />
+//         </div>
+//     );
+// }
+
+// export default App;
+
+import React, { Component } from 'react'
+// import CBCPropsEx from './src./compnents./propsex./CBCPropsEx';
+import CBCPropsEx from './components/propsex/CBCPropsEx'
+import FBCPropEx from './components/propsex/FBCPropEX'
+
+export default class App extends Component {
+  render() {
     return (
-        <div className='App'>
-            <Navbar />
-            <Main />
-            <div className='Sidebar'>
-                <Sidebar1 />
-                <Sidebar2 />
-            </div>
-            <Footer />
-        </div>
-    );
+      <div>
+        <CBCPropsEx
+        username="sanju"
+        age={20}
+        hobbies={["playing" , "reading books" , "stock"]}
+        address={{city:"kamareddy" , area:"dmk"}}
+        sendFun={function(){alert("hi i am sanju")}}
+        />
+
+      </div>
+    )
+  }
 }
 
-export default App;
+
+<FBCPropEx
+username="Sanju"
+isMarried={true}
+hobbies={["Reading","Writing","Eating"]}
+/>
+
+
 
---
+++ src/components/propsex/Child3.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Child3 extends Component {
+  render() {
+    return (
+      <div>Child3</div>
+    )
+  }
+}
